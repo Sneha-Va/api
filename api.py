3a872432b1ad1a07d3828dfc3fac43e8c5faad60
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-03 16:54:54 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,21 @@
+
+import requests
+import json
+import mysql.connector
+try:
+    mydb=mysql.connector.connect(host='localhost',user='root',password='',database='userdb')
+except mysql.connector.Error as e:
+    print("sql connection error",e)
+mycursor = mydb.cursor()
+
+data=requests.get("https://jsonplaceholder.typicode.com/users").text
+data_info=json.loads(data)
+#print(data_info)
+#user_list=[]
+for i in data_info:
+    #user_list.append([i["name"],i["email"],i["phone"]])
+    sql="INSERT INTO `user`(`name`, `email`, `phoneno`) VALUES ('"+i['name']+"','"+i['email']+"','"+i['phone']+"')"
+    mycursor.execute(sql)
+    mydb.commit()
+    print("data inserted sucessfully"+i["name"])
+#print(user_list)
